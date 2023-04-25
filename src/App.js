9e5cb3bbc1f6165a3e3949f00bbfc651b44059fd
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-04-25 21:22:40 +0530

    added fetch data func

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import { useEffect } from 'react';
 import './styles/App.css';
 
 function App() {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://api.openai.com/v1/completions", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          model: "text-davinci-003",
+          prompt: "Hi, how are you today?",
+          max_tokens: 7,
+        })
+      })
+      const data = await response.json()
+      console.log(data)
+    }
+
+    fetchData()
+  }, [])
   return (
     <div className="App">
       Hi
