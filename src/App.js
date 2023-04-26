5e48c715834f3a5beac8ea6c361d7970cdf2c611
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-04-26 10:44:28 +0530

    - added fetch data using axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './styles/App.css';
+import axios from 'axios';
 
 function App() {
 
+  const [data, setData] = useState(null)
+
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch("https://api.openai.com/v1/completions", {
-        method: "POST",
+      const config = {
         headers: {
-          Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          model: "text-davinci-003",
-          prompt: "Hi, how are you today?",
-          max_tokens: 7,
-        })
-      })
-      const data = await response.json()
+          'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+          'Content-Type': 'application/json',
+        }
+      }
+      const sendData = {
+        model: "text-davinci-003",
+        prompt: "Hi, how are you today?",
+        max_tokens: 7,
+      }
+      const response = await axios.post(process.env.REACT_APP_API_URL, sendData, config)
+      const res = response.data
+      setData(res)
       console.log(data)
     }
 
